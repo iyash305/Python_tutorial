0f44f4cd8255f8d804091e79c5eae2acc3183fcb
Yash <yindulkar30@gmail.com>
2025-09-20 08:04:51 +0530

    update

--- lists.py
+++ lists.py
@@ -3,3 +3,13 @@ print(friends)
 print(friends[-1])
 print(friends.count("Yash"))
 print(len(friends))
+friends.reverse()
+print(friends)
+friends.sort()
+print(friends)
+
+cars = [911,130,328,900,750,535]
+cars.sort()
+print(cars)
+friends.pop()
+print(friends)
