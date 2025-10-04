bae7921e3abf7fe958d22bd55526e3596f39af3e
Yash <yindulkar30@gmail.com>
2025-10-04 09:15:51 +0530

    func_exe

--- func.py
+++ func.py
@@ -1,7 +1,9 @@
-def greeting(name,age=25):
-    print("Hello " + name, "you are " + str(age) + " years old")
-    print(f"Hello {name}! you are {age} years old")
+def greeting(name,age=25,colour ="red"):
+    print("Hello " + name.capitalize(), " you will be " + str(age+1) + " next birthday")
+    print(f"Hello {name.capitalize()}! you will be  {age+1} next birthday")
+    print(f"We heard you like the colour {colour.lower()}")
 name = input("Enter your name:")
-greeting("John",27)
-greeting("Diana")
-greeting(name,26)
+age = input("Enter your age: ")
+colour = input("Enter your favourite colour: ")
+
+greeting("Yash",int(age),colour)
