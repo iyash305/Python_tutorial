1d8a1bdc3bb8d490b69fb316d0adb4a6294881d5
Yash <yindulkar30@gmail.com>
2025-11-04 16:56:38 +0530

    major changes

---
+++ comments/01_basics.py
@@ -0,0 +1,3 @@
+print("Hello World", "this is Yash",sep="..")
+
+print("Hello, Python Learner \nI am \"good\"")
---
+++ user_input/basic_user_input.py
@@ -0,0 +1,8 @@
+# a = input("Enter a number")
+# int(a)
+# print(a + 12)
+
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+print(a + b)
+
